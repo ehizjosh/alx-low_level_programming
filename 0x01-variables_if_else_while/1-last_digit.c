6b29c508c2ccb67cba93022d3db11dc85c2ba590
godfrey ilogho <godfreyilogho@gmail.com>
2023-08-13 15:49:36 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,17 +1,16 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
- * main - Prints a text according to number
+ * main - Prints a text according number
  *
- * Return: Always (success)
+ * Return: Always (Success)
  */
-
 int main(void)
 {
-	int n, lastd
-	
+	int n, lastd;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	lastd = n % 10;
