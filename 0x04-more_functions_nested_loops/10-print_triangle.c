14ebdc82e16b59ec1f41f7d06b0a7fbf2880a97a
godfrey ilogho <godfreyilogho@gmail.com>
2023-08-18 15:36:12 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -25,8 +25,9 @@ void print_triangle(int size)
 			}
 			for (hashes = 1; hashes <= row; hashes++)
 			{
-			_putchar('#');
+				_putchar('#');
 			}
-		}	_putchar('\n');
+			_putchar('\n');
+		}
 	}
 }
