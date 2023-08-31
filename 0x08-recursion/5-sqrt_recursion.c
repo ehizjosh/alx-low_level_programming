623e4231b46c15e5c842b1fef509e6c720e49614
godfrey ilogho <godfreyilogho@gmail.com>
2023-08-31 02:26:00 -0700

    fix 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,21 +1,17 @@
 #include "main.h"
+int sqrt_helper(int n, int guess);
 /**
-* _pow_recursion - calculates the power of x raised to power of y
-* @x: the base value
-* @y: the exponent value
-* Return: The result of x raised to the power of y, or -1.
+* _sqrt_recursion - calculates the natural square root of a number
+* @n: The number for which to find the square root
+* Return: The natural square root of n, or -1
 */
-int _pow_recursion(int x, int y)
+int _sqrt_recursion(int n)
 {
-	if (y < 0)
+	if (n < 0)
 	{
 		return (-1);
 	}
-	if (y == 0)
-	{
-		return (1);
-	}
-	return (x * _pow_recursion(x, y - 1));
+	return (sqrt_helper(n, 1));
 }
 /**
 * sqrt_helper - Recursive helper function to find the square root
